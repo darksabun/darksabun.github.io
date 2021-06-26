a99a49a3b12585961476ac8397187e0ea8fe62c4
darksabun <62116388+darksabun@users.noreply.github.com>
2021-06-26 23:56:04 +0900

    for LR2IR Battle

--- docs/event/irbattle/js/battle_table.js
+++ docs/event/irbattle/js/battle_table.js
@@ -1,5 +1,7 @@
+// Battle Ranking Table
 function makeBattleTable() {
-    $("#table_int").DataTable({
+    table = $("#table_int").DataTable({
+        destroy: true,
         paging: false,
         info: false,
         lengthChange: false,
@@ -72,3 +74,28 @@ var tableColumns = [
 ];
     
 makeBattleTable();
+
+// Table Language Change
+function tableLangKO() {
+    $(".table-responsive").empty();
+    $(".table-responsive").append('<table id="table_int" class="table table-striped table-bordered table-hover text-gray-900 text-center"></table>');
+    $("#table_int").append("<thead><tr><th rowspan='2'>플레이어</th><th colspan='2' class='table-success'>초급자</th><th colspan='2' class='table-warning'>중급자</th><th colspan='2' class='table-danger'>상급자</th><th rowspan='2'>합계</th></tr><tr><th>연타</th><th>난타</th><th>동시치기</th><th>스크래치</th><th>트릴</th><th>롱놋</th></tr></thead>");
+    makeBattleTable();
+    changeLangKO();
+}
+
+function tableLangEN() {
+    $(".table-responsive").empty();
+    $(".table-responsive").append('<table id="table_int" class="table table-striped table-bordered table-hover text-gray-900 text-center"></table>');
+    $("#table_int").append("<thead><tr><th rowspan='2'>Player</th><th colspan='2' class='table-success'>Beginner</th><th colspan='2' class='table-warning'>Intermediate</th><th colspan='2' class='table-danger'>Expert</th><th rowspan='2'>Total</th></tr><tr><th>Jacks</th><th>Stream</th><th>Chords</th><th>Scratch</th><th>Trill</th><th>LN</th></tr></thead>");
+    makeBattleTable();
+    changeLangEN();
+}
+
+function tableLangJP() {
+    $(".table-responsive").empty();
+    $(".table-responsive").append('<table id="table_int" class="table table-striped table-bordered table-hover text-gray-900 text-center"></table>');
+    $("#table_int").append("<thead><tr><th rowspan='2'>プレイヤー</th><th colspan='2' class='table-success'>初級者</th><th colspan='2' class='table-warning'>中級者</th><th colspan='2' class='table-danger'>上級者</th><th rowspan='2'>合計</th></tr><tr><th>連打</th><th>乱打</th><th>同時押し</th><th>皿</th><th>トリル</th><th>LN</th></tr></thead>");
+    makeBattleTable();
+    changeLangJP();
+}
