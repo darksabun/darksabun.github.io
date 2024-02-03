7235a83c06d7c5794a9a8ea42d212a57bc8eab88
darksabun <62116388+darksabun@users.noreply.github.com>
2024-02-04 00:08:37 +0900

    M-SPIN's BOFXVI Diary
    http -> https.

--- docs/assets/js/venue_content.js
+++ docs/assets/js/venue_content.js
@@ -48,38 +48,34 @@ async function fetchJson() {
       iframeVideo.classList.add("rounded-3");
       iframeVideo.setAttribute(
         "srcdoc",
-        "<style>" +
-          "* {" +
-          "padding: 0;" +
-          "margin: 0;" +
-          "overflow: hidden" +
-          "}" +
-          "html,body {" +
-          "height: 100%" +
-          "}" +
-          "img,span {" +
-          "position: absolute;" +
-          "width: 100%;" +
-          "top: 0;" +
-          "bottom: 0;" +
-          "margin: auto" +
-          "}" +
-          "span {" +
-          "height: 1.5em;" +
-          "text-align: center;" +
-          "font: 48px/1.5 sans-serif;" +
-          "color: white;" +
-          "text-shadow: 0 0 0.5em black" +
-          "}" +
-          "</style>" +
-          "<a href=https://www.youtube.com/embed/" +
-          i.ds_video +
-          "?autoplay=1>" +
-          "<img src=https://img.youtube.com/vi/" +
-          i.ds_video +
-          "/hqdefault.jpg>" +
-          "<span>▶</span>" +
-          "</a>"
+        `<style>
+          * {
+            padding: 0;
+            margin: 0;
+            overflow: hidden;
+          }
+          html,body {
+            height: 100%;
+          }
+          img,span {
+            position: absolute;
+            width: 100%;
+            top: 0;
+            bottom: 0;
+            margin: auto;
+          }
+          span {
+            height: 1.5em;
+            text-align: center;
+            font: 48px/1.5 sans-serif;
+            color: white;
+            text-shadow: 0 0 0.5em black;
+          }
+        </style>
+        <a href="https://www.youtube.com/embed/${i.ds_video}?autoplay=1">
+          <img src="https://img.youtube.com/vi/${i.ds_video}/hqdefault.jpg">
+          <span>▶</span>
+        </a>`
       );
       iframeVideo.setAttribute(
         "src",
