cf2df4ec52039ac003eb927fee221218cfb18693
darksabun <62116388+darksabun@users.noreply.github.com>
2024-02-13 15:29:42 +0900

    1998 to 2024
    아무튼 해결 (2)

--- docs/assets/js/multi_lang.js
+++ docs/assets/js/multi_lang.js
@@ -1,24 +1,37 @@
 // for Multiple Language
 let getLanguage = navigator.language;
-const languages = {
-  ko: document.querySelectorAll(".lang-ko"),
-  ja: document.querySelectorAll(".lang-ja"),
-  en: document.querySelectorAll(".lang-en"),
-};
+setTimeout(() => {
+  const languages = {
+    ko: document.querySelectorAll(".lang-ko"),
+    ja: document.querySelectorAll(".lang-ja"),
+    en: document.querySelectorAll(".lang-en"),
+  };
 
-function setLanguageVisibility(targetLang) {
+  function setLanguageVisibility(nowLang) {
+    Object.values(languages).forEach((lang) => {
+      lang.forEach((element) => {
+        const langClass = element.classList.contains(`lang-${nowLang}`);
+        element.classList.toggle("text-secret", !langClass);
+      });
+    });
+  }
+
+  // Language setting based on navigator.language
+  const nowLang = getLanguage.slice(0, 2);
+  setLanguageVisibility(nowLang);
+}, 200);
+
+function changeLanguage(wantLang) {
+  const languages = {
+    ko: document.querySelectorAll(".lang-ko"),
+    ja: document.querySelectorAll(".lang-ja"),
+    en: document.querySelectorAll(".lang-en"),
+  };
+
   Object.values(languages).forEach((lang) => {
     lang.forEach((element) => {
-      const langClass = element.classList.contains(`lang-${targetLang}`);
+      const langClass = element.classList.contains(`lang-${wantLang}`);
       element.classList.toggle("text-secret", !langClass);
     });
   });
-}
-
-// Initial language setting based on navigator.language
-const initialLang = getLanguage.slice(0, 2);
-setLanguageVisibility(initialLang);
-
-function changeLanguage(lang) {
-  setLanguageVisibility(lang);
 }
--- docs/assets/js/venue_content.js
+++ docs/assets/js/venue_content.js
@@ -2,28 +2,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   if (typeof venueManualLoad === "undefined") {
     fetchJson();
-    setTimeout(() => {
-      // for Multiple Language
-      let getLanguage = navigator.language;
-      const languages = {
-        ko: document.querySelectorAll(".lang-ko"),
-        ja: document.querySelectorAll(".lang-ja"),
-        en: document.querySelectorAll(".lang-en"),
-      };
-
-      function setLanguageVisibility(targetLang) {
-        Object.values(languages).forEach((lang) => {
-          lang.forEach((element) => {
-            const langClass = element.classList.contains(`lang-${targetLang}`);
-            element.classList.toggle("text-secret", !langClass);
-          });
-        });
-      }
-
-      // Initial language setting based on navigator.language
-      const initialLang = getLanguage.slice(0, 2);
-      setLanguageVisibility(initialLang);
-    }, 300);
   }
 });
 
