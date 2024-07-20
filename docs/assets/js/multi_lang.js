8a92afdc2fe45cfc8b4111a8f6a99c07cca129f0
darksabun <62116388+darksabun@users.noreply.github.com>
2024-07-21 08:39:17 +0900

    DARKSABUN | Multi-Language support

--- docs/assets/js/multi_lang.js
+++ docs/assets/js/multi_lang.js
@@ -1,26 +1,28 @@
-// for Multiple Language
-const getLanguage = navigator.language;
+// for Multi-Language Support
 const languages = {
   ko: document.querySelectorAll(".lang-ko"),
   ja: document.querySelectorAll(".lang-ja"),
   en: document.querySelectorAll(".lang-en"),
 };
 
+function getCurrentLanguage() {
+  const browserLanguage = navigator.language.slice(0, 2);
+  return ['ko', 'ja'].includes(browserLanguage) ? browserLanguage : 'en';
+}
+
 function setLanguageVisibility(nowLang) {
-  Object.values(languages).forEach((lang) => {
-    lang.forEach((element) => {
+  Object.values(languages).forEach(lang => {
+    lang.forEach(element => {
       const langClass = element.classList.contains(`lang-${nowLang}`);
       element.classList.toggle("text-secret", !langClass);
     });
   });
 }
 
-// Language setting based on navigator.language
-let nowLang = getLanguage.slice(0, 2);
+let nowLang = getCurrentLanguage();
 setLanguageVisibility(nowLang);
 
 function changeLanguage(wantLang) {
-  // Multiple Language for Venue
   if (document.querySelector(".venue-content")) {
     document.querySelector(".venue-content").innerHTML = "";
     document.querySelector("#venue-content-load").style.display = "";
